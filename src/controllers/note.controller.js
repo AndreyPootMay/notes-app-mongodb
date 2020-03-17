786efcacfbc16a927a1e2fb7fcdc2cebc5cd3b37
AndreyPootMay <spocker312@gmail.com>
2020-03-16 21:42:56 -0500

    14 - Connect flash

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -11,24 +11,44 @@ noteCrtl.createNewNote = async (req, res) => {
     const newNote = new Note({title,description});
     
     await newNote.save();
-    
+    req.flash('success_msg', 'Note added successfully');
     res.send('adding new note');
 }
 
-noteCrtl.renderNotes = (req, res) => {
-    res.send('rendering all notes');
+noteCrtl.renderNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/all-notes', {
+        notes 
+    });
 }
 
-noteCrtl.renderEditForm = (req, res) => {
-    res.send('editing note');
+noteCrtl.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id);
+
+    res.render('notes/edit-note', {
+        note
+    });
 }
 
-noteCrtl.updateNote = (req, res) => {
-    res.send('updating note');
+noteCrtl.updateNote = async (req, res) => {
+    const {title, description} = req.body;
+
+    await Note.findByIdAndUpdate(req.params.id, {
+        title,
+        description
+    });
+
+    req.flash('success_msg', 'Note updated successfully');
+
+    res.redirect('/notes');
 }
 
-noteCrtl.deleteNote = (req, res) => {
-    res.send('deleting note');
+noteCrtl.deleteNote = async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+
+    req.flash('success_msg', 'Note deleted successfully');
+
+    res.redirect('/notes');
 }
 
 module.exports = noteCrtl;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // Initialization
 const app = express();
@@ -21,8 +24,20 @@ app.set('view engine', '.hbs');
 // Middlewares
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 
 // Global Variables
+app.use((req, res, next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+
+    next();
+});
 
 // Routes
 app.use(require('./routes/index.routes'));
